7b89f8ae39d62b102ce00a55aa4164bc93cca0df
Etienne Vautherin <etienne@vautherin.com>
2022-02-04 11:01:35 +0100

    SettingsView in LocationView ©Pierre

--- Location/LocationView.swift
+++ Location/LocationView.swift
@@ -11,30 +11,43 @@ import MapKit
 struct LocationView: View {
     @EnvironmentObject var model: ViewModel
     
+    @State var isSsettingsActive = false
+    
     var body: some View {
-        ZStack(alignment: .topTrailing) {
-            ZStack {
-                    Map(coordinateRegion: $model.userCoordinateRegion)
-                    Circle()
-                        .fill(Color.blue)
-                        .frame(width: 20.0, height: 20.0)
-                    
+        VStack {
+            ZStack(alignment: .topTrailing) {
+                ZStack {
+                        Map(coordinateRegion: $model.userCoordinateRegion)
+                        Circle()
+                            .fill(Color.blue)
+                            .frame(width: 20.0, height: 20.0)
+                        
+                    }
+                    .onAppear(perform: {
+                        LocationService.shared.start(model: model)
+                    })
+                    .onDisappear(perform: {
+                        LocationService.shared.stop()
+                    })
+                
+                Button {
+                    print("Geofencing")
+                    isSsettingsActive = !isSsettingsActive
+                } label: {
+                    if (isSsettingsActive) {
+                        Image(systemName: "xmark.circle")
+                    } else {
+                        Image(systemName: "circle.dashed")
+                    }
                 }
-                .onAppear(perform: {
-                    LocationService.shared.start(model: model)
-                })
-                .onDisappear(perform: {
-                    LocationService.shared.stop()
-                })
-            
-            Button {
-                print("Geofencing")
-            } label: {
-                Image(systemName: "circle.dashed")
-            }
-            .buttonStyle(.borderedProminent)
-            .padding()
+                .buttonStyle(.borderedProminent)
+                .padding()
 
+            }
+
+            if (isSsettingsActive) {
+                SettingsView()
+            }
         }
     }
 }
--- Location/SettingsView.swift
+++ Location/SettingsView.swift
@@ -10,28 +10,28 @@ import MapKit
 
 struct SettingsView: View {
     @EnvironmentObject var model: ViewModel
-    @State var coordinateRegion = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 0, longitude: 0),
-        latitudinalMeters: 5000.0,
-        longitudinalMeters: 5000.0
-    )
+//    @State var coordinateRegion = MKCoordinateRegion(
+//        center: CLLocationCoordinate2D(latitude: 0, longitude: 0),
+//        latitudinalMeters: 5000.0,
+//        longitudinalMeters: 5000.0
+//    )
     @State var name: String = ""
     @State var meter = 5000.0
     @State var geofencing: Bool = false
     
     var body: some View {
         VStack{
-            ZStack {
-                Map(coordinateRegion: $coordinateRegion)
-                Circle()
-                    .strokeBorder(Color.white, lineWidth: 3)
-                    .frame(width: 22.0, height: 22.0)
-                Rectangle()
-                    .frame(width: 22, height: 2, alignment: .center)
-                Rectangle()
-                    .frame(width: 2, height: 22, alignment: .center)
-                
-            }
+//            ZStack {
+//                Map(coordinateRegion: $coordinateRegion)
+//                Circle()
+//                    .strokeBorder(Color.white, lineWidth: 3)
+//                    .frame(width: 22.0, height: 22.0)
+//                Rectangle()
+//                    .frame(width: 22, height: 2, alignment: .center)
+//                Rectangle()
+//                    .frame(width: 2, height: 22, alignment: .center)
+//
+//            }
             VStack{
                 TextField("Name of your region", text: $name)
                 Text("Meter: \(Int(meter))")
